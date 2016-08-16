8937f2be54794eb515527e59af455a8c2db5dc1d
Nikola <jankovic.gd@gmail.com>
2016-08-16 15:08:00 +0200

    new function for id and new modal

--- js/mapFunc.js
+++ js/mapFunc.js
@@ -18,11 +18,17 @@ function prikazModalWFS() {
   $("#WFSMODAL").modal();
 }
 
+function prikazStatistika() {
+  $("#statpodaci").modal();
+}
+
 // klikom na dugme GENWMS pokrece se getWMS funkcija
 $("#GENWMS").on("click", getWMS);
 
 // klikom na dugme GENWMS pokrece se generisanjeWMS funkcija
 $("#GENWFS").on("click", getWFS);
+
+$("#statID").on("click", getstat);
 
 // funkcija uzima vrednost iz liste i generise link pomocu ekstenzije i vrednosti iz liste
 function getWMS(){
@@ -48,4 +54,15 @@ function getWFS(){
               "&request=GetFeature" +
               "&typeName=LPIS:" + values +
               "&maxFeatures=50",'_blank');
+}
+
+// funkcija koja uzima unet id i generise novu stranicu
+function getstat(){
+  var values = $("#identifikatorkorisnik").val();
+  try {
+    if (values == "") throw "empty";
+  } catch (e) {
+    alert("Input is " + e);
+  }
+  console.log(values);
 }
