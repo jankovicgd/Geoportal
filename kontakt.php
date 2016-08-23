f71191d88d91d7ef68b7a747f025485214506a52
Nikola <jankovic.gd@gmail.com>
2016-08-23 12:49:56 +0200

    kontakt + onama fill

--- kontakt.php
+++ kontakt.php
@@ -13,8 +13,26 @@
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-lg-12 text-left">
-					<h1>In progress</h1>
+					<h1>Kontakt</h1>
 					<hr>
+          <h2>Informacije</h2>
+          <p>
+            Ime ulice 5/50, 11000 Novi Sad
+          </p>
+          <p>
+            Br telefona: +3811111111
+          </p>
+          <p>
+            Faks: +3811111111
+          </p>
+          <p>
+            Email: <a href="#">kontakt@kontakt.kon</a>
+          </p>
+          <h2>Mapa</h2>
+            <script src='https://maps.googleapis.com/maps/api/js?v=3.exp'></script>
+              <div style='overflow:hidden;height:440px;width:700px;'><div id='gmap_canvas' style='height:440px;width:700px;'></div><div><small><a href="http://www.embedgooglemaps.com">Generate your Google map here, quick and easy!</a></small></div><style>#gmap_canvas img{max-width:none!important;background:none!important}</style>
+              </div><script type='text/javascript'>function init_map(){var myOptions = {zoom:10,center:new google.maps.LatLng(45.255785591294,19.836453838501033),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(45.255785591294,19.836453838501033)});infowindow = new google.maps.InfoWindow({content:'<strong>Title</strong><br>Trg Dositeja obradovica<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);
+            </script>
 				</div>
 			</div>
 		</div>
--- oNama.php
+++ oNama.php
@@ -13,8 +13,17 @@
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-lg-12 text-left">
-					<h1>In progress</h1>
+					<h1>O nama</h1>
 					<hr>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus enim eget ante tincidunt ullamcorper. Aliquam neque diam, facilisis ac commodo non, molestie nec eros. Phasellus cursus nibh consequat, maximus tortor ut, luctus ex. Ut pharetra, diam non posuere vestibulum, metus nunc placerat justo, nec facilisis tellus turpis in orci. Praesent rutrum diam non ex gravida, imperdiet hendrerit justo bibendum. Ut sed ex sapien. Vestibulum tempus, sem ac facilisis aliquam, sapien risus fringilla mi, id egestas ipsum tortor eget nisl. Vivamus in sem tristique, iaculis tellus vitae, maximus nunc. Sed euismod odio justo, laoreet pulvinar ipsum fermentum sit amet. Aliquam ornare nec odio vitae imperdiet. Praesent semper et mauris non consectetur. Nullam finibus viverra ipsum, ut imperdiet dolor fringilla a.
+          </p>
+          <p>
+            Duis lectus risus, tempus at felis id, ultricies volutpat neque. In dui dui, condimentum sit amet leo tempor, ornare laoreet arcu. Cras commodo quam non mi varius vestibulum. Ut pharetra, est non lobortis tristique, enim odio viverra nulla, sit amet suscipit arcu mauris ut est. Aenean a mauris auctor, pretium metus sit amet, porttitor odio. Fusce nec commodo ante. Vestibulum luctus sed nibh sit amet porta. Cras tristique cursus purus. Duis eget tellus est.
+          </p>
+          <p>
+            Fusce ac elementum tortor, eget congue tellus. Vivamus ex lacus, ornare nec ex in, lobortis bibendum turpis. Sed fringilla sem vitae eros malesuada, a tempor tortor efficitur. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer in pharetra quam. Nunc varius sapien et dolor bibendum, et interdum nibh auctor. Mauris venenatis pharetra diam, fermentum tempor eros blandit sit amet. Proin quis luctus risus. Donec interdum, erat vel venenatis auctor, velit ante vestibulum felis, sit amet aliquet tortor erat eu nisi. Vivamus mattis varius felis, ac eleifend odio malesuada eu.
+          </p>
 				</div>
 			</div>
 		</div>
